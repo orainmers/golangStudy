eef4ad5c460be9da2f06841afbb6e63ce68f8551
orainmers <arnautvladislav5@gmail.com>
2024-09-06 00:02:19 +0300

    feat: add app, store, models, docker-compose.yml

--- README.md
+++ README.md
@@ -18,3 +18,10 @@ https://golangci-lint.run/
 ```shell
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.59.1
 ```
+
+for macOS:
+
+```shell
+brew install golangci-lint
+brew upgrade golangci-lint
+```
--- internal/server/server.go
+++ internal/server/server.go
@@ -13,17 +13,24 @@ const pkgName = "server"
 type Server struct {
 	lg     *slog.Logger
 	server *http.Server
+
+	app app
 }
 
-func New(lg *slog.Logger, addr string) *Server {
+func New(lg *slog.Logger, addr string, app app) *Server {
 	s := Server{
-		lg: lg.WithGroup(pkgName),
+		lg:  lg.WithGroup(pkgName),
+		app: app,
 	}
 
 	r := chi.NewRouter()
 	r.Route("/api", func(r chi.Router) {
 		r.Route("/v1", func(r chi.Router) {
 			r.Get("/time", s.getTimeHandler)
+
+			r.Route("/person", func(r chi.Router) {
+				r.Post("/add", s.addPersonHandler)
+			})
 		})
 	})
 
@@ -37,5 +44,6 @@ func New(lg *slog.Logger, addr string) *Server {
 }
 
 func (s *Server) Run() error {
+	s.lg.Info("http server is running...")
 	return s.server.ListenAndServe()
 }
